4560d046efe783ee5854c1fdf47fe6a72a1f586b
mochimochi510 <ayaha.mochimochi55@docomo.ne.jp>
2025-06-09 16:02:11 +0900

    support for ()

--- class3/homework1.py
+++ class3/homework1.py
@@ -33,6 +33,14 @@ def read_slash(line, index):
     token = {'type': 'SLASH'}
     return token, index + 1
 
+def read_open_paren(line, index):
+    token = {'type': 'OPEN_PAREN'}
+    return token, index + 1
+
+def read_close_paren(line, index):
+    token = {'type': 'CLOSE_PAREN'}
+    return token, index + 1
+
 
 def tokenize(line):
     tokens = []
@@ -48,6 +56,10 @@ def tokenize(line):
             (token, index) = read_times(line, index)
         elif line[index] == '/':
             (token, index) = read_slash(line, index)
+        elif line[index] == '(':
+            (token, index) = read_open_paren(line, index)
+        elif line[index] == ')':
+            (token, index) = read_close_paren(line, index)       
         else:
             print('Invalid character found: ' + line[index])
             exit(1)
@@ -55,7 +67,7 @@ def tokenize(line):
     return tokens
 
 
-def evaluate(tokens):
+def evaluate_without_parens(tokens):
     answer = 0
     tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
     # 積・商の計算
@@ -85,10 +97,33 @@ def evaluate(tokens):
         index += 1
     return answer
 
+def evaluate_total(tokens):
+    answer = 0
+    tokens.insert(0, {'type': 'PLUS'})
+    # 括弧の内部の値を計算する
+    index = 1
+    while index < len(tokens):
+        if tokens[index]['type'] == 'CLOSE_PAREN':
+            # )に対応する()内の値を計算
+            open_parens_index = 0
+            for i in range(index, -1, -1):
+                if tokens[i]['type'] == 'OPEN_PAREN':
+                    open_parens_index = i
+                    break
+            part_of_tokens = tokens[open_parens_index+1:index] 
+            del tokens[open_parens_index:index+1]
+            tokens.insert(open_parens_index, {'type': 'NUMBER', 'number': evaluate_without_parens(part_of_tokens)})
+            index = open_parens_index + 1
+        else:
+            index += 1
+    print(tokens)
+    # 括弧が全てはずれた式を計算する
+    return evaluate_without_parens(tokens)
+            
 
 def test(line):
     tokens = tokenize(line)
-    actual_answer = evaluate(tokens)
+    actual_answer = evaluate_total(tokens)
     expected_answer = eval(line)
     if abs(actual_answer - expected_answer) < 1e-8:
         print("PASS! (%s = %f)" % (line, expected_answer))
@@ -108,6 +143,11 @@ def run_test():
     test("2.5*4/7")
     test("3*4+5")
     test("1-2/4+3*5")
+    test("(2-1)")
+    test("(2-1)*4")
+    test("3.0+4*(2-1)")
+    test("((2-1)*5)")
+    test("(3.0+4*(2-1))/5")
     print("==== Test finished! ====\n")
 
 run_test()
@@ -116,5 +156,5 @@ while True:
     print('> ', end="")
     line = input()
     tokens = tokenize(line)
-    answer = evaluate(tokens)
+    answer = evaluate_total(tokens)
     print("answer = %f\n" % answer)
