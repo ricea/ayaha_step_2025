30960ef530ac32a9ecdb394db2e67e1ed1fedc3c
mochimochi510 <ayaha.mochimochi55@docomo.ne.jp>
2025-06-28 23:00:30 +0900

    add 2-opt

--- class5/my_solver.py
+++ class5/my_solver.py
@@ -29,12 +29,37 @@ def solve(cities):
             next_city = min(unvisited_cities,
                             key=lambda city: dist[current_city][city])
         else:
-            next_city_index = rd.randint(0,len(unvisited_cities)-1)
-            next_city = unvisited_cities(next_city_index)
+            next_city = rd.choice(list(unvisited_cities))
         unvisited_cities.remove(next_city)
         tour.append(next_city)
         current_city = next_city
-    return tour
+        
+    # 2-opt
+    while True:
+        switch = False
+        size = len(tour)
+        for i in range(size-2):
+            # p1,p2は連続する2点のindex
+            p1 = i
+            p2= i + 1
+            for j in range(p2+1, size):
+                p3 = j
+                # p3がリストの1番最後の点の場合ははじめの点を指定
+                p4 = (j+1) % size
+                
+                # 4点で線分を入れ替えた場合に距離が短くなる場合は入れ替える
+                l1 = distance(cities[tour[p1]], cities[tour[p2]])
+                l2 = distance(cities[tour[p3]], cities[tour[p4]])
+                l3 = distance(cities[tour[p1]], cities[tour[p3]])
+                l4 = distance(cities[tour[p2]], cities[tour[p4]])
+                if l1+l2 > l3+l4:
+                    # 点を入れ替え
+                    new_path = tour[p2:p3+1]
+                    tour[p2:p3+1] = new_path[::-1]
+                    switch = True
+        if not switch:
+            break                     
+    return tour 
 
 
 if __name__ == '__main__':
